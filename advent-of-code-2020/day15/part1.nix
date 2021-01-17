8193b8845516c166353c56ceadf4d1eb8b8341e2
oren <countoren@gmail.com>
2021-01-17 17:07:31 -0500

    Update part1.nix
    remove not needed src

--- advent-of-code-2020/day15/part1.nix
+++ advent-of-code-2020/day15/part1.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs>{};
-runCommand "AOC-Day15-P1" { src = ./main.hs; buildInputs = [ ghc ]; } ''
+runCommand "AOC-Day15-P1" { buildInputs = [ ghc ]; } ''
   runghc ${./main.hs} > $out
   echo 'cat result for seeing the result'
 ''
