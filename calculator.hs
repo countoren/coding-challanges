bc96f632fc128949455982d545e81ef36f8ccec1
Joseph <joe@calledtoconstruct.net>
2021-01-30 22:43:28 -0500

    Adding proposed doctest comments.
    The intention here is to provide both test cases and sample code.
    Hopefully the sample code makes it easier to comprehend how pieces of the code are used / useful.

--- calculator.hs
+++ calculator.hs
@@ -1,13 +1,35 @@
 module Calculator where
 import Text.ParserCombinators.ReadP
 
+-- | Evaluate an expression
+-- From: https://www.codewars.com/kata/5235c913397cbf2508000048
+--
+-- given the following expression, the result is...
+-- >>> evaluate "10 + (30 * 2) + 3"
+-- 73
+--
+-- >>> evaluate "(10 + 30) * (2 + 3)"
+-- 240
+--
 evaluate :: String -> Double
 evaluate = fst . last . readP_to_S p . filter (/=' ')
 
+-- | Expression parser
+-- >>> ( \[(_,_),(op,_)] -> op ) $ readP_to_S p "3+4"
+-- 7.0
+--
 p = (`chainl1` add) . (`chainl1` mul) $ choice [ number, between (char '(') (char ')') p]
 
 number :: ReadP Double
 number = read <$> munch1 (\c-> (elem c ['0'..'9']) || c == '.')
 
+-- | Add parser
+-- >>> ( \[(op, "")] -> op 3 4 ) $ readP_to_S add "+"
+-- 7
+--
+-- >>> ( \[(op, "")] -> op 4 3 ) $ readP_to_S add "-"
+-- 1
+--
 add = choice [(+) <$ char '+', (-) <$ char '-']
+
 mul = choice [(*) <$ char '*', (/) <$ char '/']
